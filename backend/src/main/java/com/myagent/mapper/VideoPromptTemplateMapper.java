4ad50ce5e4b4caa70ce0c983f38bc68b675bdce4
zhangkk-art <zhangkai.202410@gmail.com>
2026-06-12 14:17:57 +0800

    feat: add VideoPromptTemplateMapper

---
+++ backend/src/main/java/com/myagent/mapper/VideoPromptTemplateMapper.java
@@ -0,0 +1,9 @@
+package com.myagent.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.myagent.model.VideoPromptTemplate;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface VideoPromptTemplateMapper extends BaseMapper<VideoPromptTemplate> {
+}
